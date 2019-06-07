a3746366ac4885976db1ef818c84b7e1d4bf0ce8
Baiardi Tjoa <tjoa.baiardi@he-arc.ch>
2019-06-07 17:09:53 +0200

    commit final

--- SuperPile/src/Exo_git/UseSuperPile.java
+++ SuperPile/src/Exo_git/UseSuperPile.java
@@ -3,28 +3,45 @@ package Exo_git;
 
 public class UseSuperPile
 	{
-
-	/*------------------------------------------------------------------*\
-	|*							Constructeurs							*|
-	\*------------------------------------------------------------------*/
-
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
 
-	/*------------------------------*\
-	|*				Set				*|
-	\*------------------------------*/
+	public static void main(String[] args)
+		{
+		main();
+		}
 
-	/*------------------------------*\
-	|*				Get				*|
-	\*------------------------------*/
+	public static void main()
+		{
+		remplirPile(7);
+		System.out.println();
+
+		SuperPile.getInstance().taille();
+		SuperPile.getInstance().sommet();
+		SuperPile.getInstance().sousSommet();
+		System.out.println();
+
+		viderPile(8);
+		SuperPile.getInstance().taille();
+		}
 
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
+	private static void remplirPile(int n)
+		{
+		for(int i = 0; i < n; i++)
+			{
+			SuperPile.getInstance().empiler(i);
+			}
+		}
 
-	/*------------------------------------------------------------------*\
-	|*							Attributs Private						*|
-	\*------------------------------------------------------------------*/
+	private static void viderPile(int n)
+		{
+		for(int i = 0; i < n; i++)
+			{
+			SuperPile.getInstance().depiler();
+			}
+		}
 	}
